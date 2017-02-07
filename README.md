132d4944d3f31d64a8b6628113eb611ba1d7a9d8
Eric Hiller <eric@hiller.pro>
2017-02-07 13:01:01 -0800

    rudimentary color; start of a readme

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Usage:
+
+
+
+```go
+import "github.com\erichiller\lgr"
+
+func actions(){
+	// actions taken, resulting in error
+	// <package>.<message level>.<Print or Println>("string")
+	lgr.ERROR.Println("I've stubbed my toe")
+}
+
+```
--- lgr.go
+++ lgr.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 // Level describes the chosen log level between
@@ -27,26 +29,33 @@ type NotePad struct {
 type Feedback struct{}
 
 const (
-	LevelTrace Level = iota
-	LevelDebug
-	LevelInfo
-	LevelWarn
-	LevelError
-	LevelCritical
-	LevelFatal
-	DefaultLogThreshold    = LevelWarn
-	DefaultStdoutThreshold = LevelError
+	LevelTrace Level = iota						// Excessive User Output
+	LevelDebug									// Detailed User Output
+	LevelInfo									// Elevated User Output
+	LevelMsg									// Standard User Output
+	LevelWarn									// Non-Critical Errors
+	LevelError									// Important Errors
+	LevelCritical								// Disrupting Errors
+	LevelFatal									// System destroying, flee the building errors
+	DefaultLogThreshold    = LevelInfo
+	DefaultStdoutThreshold = LevelMsg
 )
+
+
+
+var colorInfo = color.New(color.FgYellow).SprintFunc()
+var colorError = color.New(color.FgRed).SprintFunc()
 
 var (
 	TRACE      *log.Logger
 	DEBUG      *log.Logger
 	INFO       *log.Logger
+	MSG        *log.Logger
 	WARN       *log.Logger
 	ERROR      *log.Logger
 	CRITICAL   *log.Logger
 	FATAL      *log.Logger
-	LOG        *log.Logger
+	LOG        *log.Logger // ?????
 	FEEDBACK   Feedback
 	LogHandle  io.Writer  = ioutil.Discard
 	OutHandle  io.Writer  = os.Stdout
@@ -56,6 +65,7 @@ var (
 	trace           *NotePad = &NotePad{Level: LevelTrace, Handle: os.Stdout, Logger: &TRACE, Prefix: "TRACE: "}
 	debug           *NotePad = &NotePad{Level: LevelDebug, Handle: os.Stdout, Logger: &DEBUG, Prefix: "DEBUG: "}
 	info            *NotePad = &NotePad{Level: LevelInfo, Handle: os.Stdout, Logger: &INFO, Prefix: "INFO: "}
+	msg            *NotePad = &NotePad{Level: LevelMsg, Handle: os.Stdout, Logger: &MSG, Prefix: "MSG: "}
 	warn            *NotePad = &NotePad{Level: LevelWarn, Handle: os.Stdout, Logger: &WARN, Prefix: "WARN: "}
 	err             *NotePad = &NotePad{Level: LevelError, Handle: os.Stdout, Logger: &ERROR, Prefix: "ERROR: "}
 	critical        *NotePad = &NotePad{Level: LevelCritical, Handle: os.Stdout, Logger: &CRITICAL, Prefix: "CRITICAL: "}
@@ -68,7 +78,7 @@ func init() {
 	SetStdoutThreshold(DefaultStdoutThreshold)
 }
 
-// initialize will setup the jWalterWeatherman standard approach of providing the user
+// initialize will setup the standard approach of providing the user
 // some feedback and logging a potentially different amount based on independent log and output thresholds.
 // By default the output has a lower threshold than logged
 // Don't use if you have manually set the Handles of the different levels as it will overwrite them.
@@ -96,42 +106,45 @@ func initialize() {
 		log.Ldate|log.Ltime|log.Lshortfile)
 }
 
-// Level returns the current global log threshold.
+// LogThreshold returns the current global log threshold.
+// Level is the current Log Level ( file output level )
 func LogThreshold() Level {
 	return logThreshold
 }
 
-// Level returns the current global output threshold.
+// StdoutThreshold returns the current global output threshold.
+// Level is the current Stdout ( terminal output level )
 func StdoutThreshold() Level {
 	return outputThreshold
 }
 
-// Ensures that the level provided is within the bounds of available levels
+// levelCheck Ensures that the level provided is within the bounds of available levels
 func levelCheck(level Level) Level {
 	switch {
-	case level <= LevelTrace:
-		return LevelTrace
-	case level >= LevelFatal:
-		return LevelFatal
-	default:
-		return level
+        case level <= LevelTrace:
+            return LevelTrace
+        case level >= LevelFatal:
+            return LevelFatal
+        default:
+            return level
 	}
 }
 
-// Establishes a threshold where anything matching or above will be logged
+// SetLogThreshold Establishes a threshold where anything matching or above will be logged
 func SetLogThreshold(level Level) {
 	logThreshold = levelCheck(level)
 	initialize()
 }
 
-// Establishes a threshold where anything matching or above will be output
+// SetStdoutThreshold Establishes a threshold where anything matching or above will be output
 func SetStdoutThreshold(level Level) {
 	outputThreshold = levelCheck(level)
 	initialize()
 }
 
-// Conveniently Sets the Log Handle to a io.writer created for the file behind the given filepath
-// Will only append to this file
+// SetLogFile Sets the Log Handle to an io.writer
+// takes a single string argument of `path` which is the path to be used as the log file
+// This file will be appended to or created
 func SetLogFile(path string) {
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
@@ -145,7 +158,7 @@ func SetLogFile(path string) {
 	initialize()
 }
 
-// Conveniently Creates a temporary file and sets the Log Handle to a io.writer created for it
+// UseTempLogFile Creates a temporary file and sets the Log Handle to a io.writer created for it
 func UseTempLogFile(prefix string) {
 	file, err := ioutil.TempFile(os.TempDir(), prefix)
 	if err != nil {
@@ -158,17 +171,19 @@ func UseTempLogFile(prefix string) {
 	initialize()
 }
 
-// Disables logging for the entire JWW system
+// DiscardLogging Disables logging
 func DiscardLogging() {
 	LogHandle = ioutil.Discard
 	initialize()
 }
 
-// Feedback is special. It writes plainly to the output while
+// Println Feedback is special. It writes plainly to the output while
 // logging with the standard extra information (date, file, etc)
 // Only Println and Printf are currently provided for this
 func (fb *Feedback) Println(v ...interface{}) {
+	color.Set(color.FgRed)
 	fmt.Println(v...)
+	color.Unset() // Don't forget to unset
 	LOG.Println(v...)
 }
 

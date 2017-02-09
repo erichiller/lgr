a227f8ef09a84645ea59b0ed8e5394ae51f79bc1
Eric Hiller <eric@hiller.pro>
2017-02-09 14:46:43 -0800

    working with custom writer!

--- lgr.go
+++ lgr.go
@@ -9,7 +9,11 @@ import (
 	"os"
 
 	"github.com/fatih/color"
+	
 )
+
+import "strings"
+// import "fmt"
 
 // Level describes the chosen log level between
 // debug and critical.
@@ -18,10 +22,9 @@ type Level int
 type LogType struct {
 	Level           Level
 	Prefix          string
-	FileLogger      *log.Logger
-	PrintLogger	    *log.Logger
+	Handle          io.Writer
+    Logger          **log.Logger
     color           *color.Color
-	Loggers         []log.Logger
 	PrintDebug      bool
 }
 
@@ -47,53 +50,72 @@ const (
 )
 
 var (
-	TRACE *LogType = &LogType{
+    TRACE *log.Logger
+	DEBUG *log.Logger
+	INFO *log.Logger
+	MSG *log.Logger
+	WARN *log.Logger
+	ERROR *log.Logger
+	CRITICAL *log.Logger
+	FATAL *log.Logger
+)
+
+var (
+	Trace *LogType = &LogType{
         Level: LevelTrace, 
         Prefix: "TRACE: ",
         color:  color.New(color.FgCyan),
         PrintDebug: true,
+        Logger: &TRACE,
     }
-	DEBUG *LogType = &LogType{
+	Debug *LogType = &LogType{
         Level: LevelDebug, 
         Prefix: "DEBUG: ",
         color:  color.New(color.FgMagenta),
         PrintDebug: true,
+        Logger: &DEBUG,
     }
-	INFO *LogType = &LogType{
+	Info *LogType = &LogType{
         Level: LevelInfo, 
         Prefix: "INFO: ",
         color:  color.New(color.FgBlue),
         PrintDebug: false,
+        Logger: &INFO,
     }
-	MSG *LogType = &LogType{
+	Msg *LogType = &LogType{
         Level: LevelMsg, 
         Prefix: "MSG: ",
         color:  color.New(color.FgWhite),
         PrintDebug: false,
+        Logger: &MSG,
     }
-	WARN *LogType = &LogType{
+	Warn *LogType = &LogType{
         Level: LevelWarn,
         Prefix: "WARN: ",
         color:  color.New(color.FgYellow).Add(color.Underline),
         PrintDebug: true,
+        Logger: &WARN,
     }
-	ERROR *LogType = &LogType{
+	Error *LogType = &LogType{
         Level: LevelError,
         Prefix: "ERROR: ",
         color:  color.New(color.FgRed),
         PrintDebug: true,
+        Logger: &ERROR,
     }
-	CRITICAL *LogType = &LogType{
+	Critical *LogType = &LogType{
         Level: LevelCritical,
         Prefix: "CRITICAL: ",
         color:  color.New(color.FgRed).Add(color.Underline),
         PrintDebug: true,
+        Logger: &CRITICAL,
     }
-	FATAL *LogType = &LogType{
+	Fatal *LogType = &LogType{
         Level: LevelFatal,
         Prefix: "FATAL: ",
         color:  color.New(color.FgRed).Add(color.Underline).Add(color.Bold),
         PrintDebug: true,
+        Logger: &FATAL,
     }
 	logThreshold    Level    = DefaultLogThreshold
 	outputThreshold Level    = DefaultStdoutThreshold
@@ -101,8 +123,23 @@ var (
     // FileHandle is the handle for the log file to write to
 	FileHandle      io.Writer  = ioutil.Discard
 
-    LogTypes        []*LogType = []*LogType{TRACE, DEBUG, INFO, MSG, WARN, ERROR, CRITICAL, FATAL}
+    LogTypes        []*LogType = []*LogType{Trace, Debug, Info, Msg, Warn, Error, Critical, Fatal}
 )
+
+func (lt LogType) Write(p []byte) (n int, err error) {
+    var str string = string(p[:])
+    var strs []string = strings.SplitN(str,":",6)
+    var msg string = str
+    if len(strs) >= 6 {
+        msg = strs[5]
+    } 
+    if !lt.PrintDebug { 
+        lt.color.Print(msg)
+    } else {
+        lt.color.Print(str)
+    }
+    return len(p), nil
+}
 
 
 // init will setup the standard approach of providing the user
@@ -111,64 +148,37 @@ var (
 // Don't use if you have manually set the Handles of the different levels as it will overwrite them.
 func init() {
 	SetStdoutThreshold(DefaultStdoutThreshold)
+    SetLogThreshold(DefaultStdoutThreshold)
+    
 }
 
-type eWriterI interface {
-    Write(p []byte) (n int, err error)
-
-}
-
-type eWriter struct {
-    eWriterI
-    color *color.Color
-
-}
-
-// Write replacement function for io.writer
-func Write(bt []byte) (n int, err error) {
-    return 3,nil
-}
 
 func refreshLogTypes(){
 	// see log flag constants
 	// https://golang.org/pkg/log/#pkg-constants
 	for _, n := range LogTypes {
-
-        // this is an actual file on the filesystem to be logged to
-		n.FileLogger = log.New(FileHandle,n.Prefix, log.Ldate|log.Ltime|log.Lshortfile)
-        // this is a custom writer for the purpose of coloration
-        colorOut := eWriter{ color:  n.color }
         
-        // this is output to the console - going to be displayed to the user - ideally colorized
-        // if the log level has PrintDebug set, the detailed log information will be included
-        if n.PrintDebug {
-            n.PrintLogger = log.New(colorOut,n.Prefix,log.Ldate|log.Ltime|log.Lshortfile)
-        } else {
-            // else if not, it will be a plain message, with only the prefix (which can be blank at the log level)
-            n.PrintLogger = log.New(colorOut,n.Prefix,0)
-        }
-		
-        // if the log level is less than the ouputThreshold (stdout)
+        // if the log level is less than the outputThreshold (stdout)
         // and less than logThreshold (file output)
         // than don't log anything
 		if n.Level < outputThreshold && n.Level < logThreshold {
-			n.Loggers = []log.Logger{}
+			n.Handle = ioutil.Discard
 		} else if n.Level >= outputThreshold && n.Level >= logThreshold {
             // if greater than or equal to both, log to both
-			n.Loggers = []log.Logger{*n.FileLogger,*n.PrintLogger}
+			n.Handle = io.MultiWriter(FileHandle, n)
 		} else if n.Level >= outputThreshold && n.Level < logThreshold {
-            // if only outputThreshold is greater, only log to PrintLogger
-			n.Loggers = []log.Logger{*n.PrintLogger}
+            // if only outputThreshold is greater, only log to console
+			n.Handle = n
 		} else {
             // else (the only option remaining is logThreshold is greater)
             // log to FileLogger only
-			n.Loggers = []log.Logger{*n.FileLogger}
+			n.Handle = FileHandle
 		}
+
+        *n.Logger = log.New(n.Handle, n.Prefix, log.Ldate|log.Ltime|log.Lshortfile)
+
 	}
 
-	// for _, n := range LogTypes {
-	// 	*n.Logger = log.New(n.Handle, n.Prefix, log.Ldate|log.Ltime|log.Lshortfile)
-	// }
 }
 
 // LogThreshold returns the current global log threshold.
@@ -242,29 +252,4 @@ func DiscardLogging() {
 	refreshLogTypes()
 }
 
-// Feedback is special. It writes plainly to the output while
-// logging with the standard extra information (date, file, etc)
-// Only Println and Printf are currently provided for this
-func (log *LogType) Print(v ...interface{}) {
-	log.color.Print(v...)
-	log.FileLogger.Print(v...)
-}
-
-// Feedback is special. It writes plainly to the output while
-// logging with the standard extra information (date, file, etc)
-// Only Println and Printf are currently provided for this
-func (log *LogType) Printf(format string, v ...interface{}) {
-	log.color.Printf(format,v...)
-	log.FileLogger.Printf(format,v...)
-}
-
-// Feedback is special. It writes plainly to the output while
-// logging with the standard extra information (date, file, etc)
-// Only Println and Printf are currently provided for this
-func (log *LogType) Println(v ...interface{}) {
-    log.color.Set()
-	log.PrintLogger.Println(v...)
-    color.Unset()
-	log.FileLogger.Println(v...)
-}
 
